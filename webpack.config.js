0f895b71fe8a51c2c88f64774d06823ec7902fd1
hassannajeeb <113021660+hassannajeeb@users.noreply.github.com>
2022-09-23 08:15:55 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
-/***
- * Excerpted from "Build Reactive Websites with RxJS",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material,
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose.
- * Visit http://www.pragmaticprogrammer.com/titles/rkrxjs for more book information.
-***/
 const path = require('path');
-
 module.exports = {
   mode: 'development',
   // defined above
  entry:{
 
-    pigLatin: './pigLatin.ts'
+    piglatin: './piglatin.ts'
 },
-devServer: {
+  devServer: {
     inline: true,
     host: '0.0.0.0',
     port: 8080,
     disableHostCheck: true
   },
+  
   module: {
     rules: [
       {
