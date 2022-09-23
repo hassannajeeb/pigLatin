62dcb570388f95f146d5c0271202c35dcfb27eec
hassannajeeb <113021660+hassannajeeb@users.noreply.github.com>
2022-09-23 08:14:59 +0500

    Update pigLatin.ts

--- pigLatin.ts
+++ pigLatin.ts
@@ -1,23 +1,27 @@
-import { fromEvent, from } from 'rxjs'; 
-import { map, mergeMap, reduce } from 'rxjs/operators'; 
-// Converts a word in English to one in Pig Latin 
-function pigLatinify(word) { 
-// Handle single-letter case and empty strings 
-if (word.length < 2) { 
-return word; 
-} 
-return word.slice(1) + '-' + word[0] + 'ay'; 
-} 
-fromEvent<any>(textbox, 'keyup') 
-.pipe( 
-map(event => event.target.value), 
-mergeMap(wordString => 
-// Inner observable 
-from(wordString.split(/\s+/)) 
-.pipe( 
-map(pigLatinify), 
-reduce((bigString, newWord) => bigString + ' ' + newWord, '') 
-) 
-) 
-) 
-.subscribe(translatedWords => results.innerText = translatedWords); 
+import { fromEvent, from } from 'rxjs';
+import { map, mergeMap, reduce } from 'rxjs/operators';
+
+let textbox = document.querySelector<HTMLTextAreaElement>('#text-input');
+let results = document.querySelector<HTMLElement>('#results');
+// Converts a word in English to one in Pig Latin
+function pigLatinify(word) {
+  // Handle single-letter case and empty strings
+  if (word.length < 2) {
+    return word;
+  }
+  return word.slice(1) + '-' + word[0] + 'ay';
+}
+// You can add tap operator inside .pipe to log the stream values
+fromEvent<any>(textbox, 'keyup')
+.pipe(
+  map(event => event.target.value),
+  mergeMap(wordString =>
+    // Inner observable
+    from(wordString.split(/\s+/))
+    .pipe(
+      map(pigLatinify),
+      reduce((bigString, newWord) => bigString + ' ' + newWord, '')
+    )
+  )
+)
+.subscribe(translatedWords => results.innerText = translatedWords);
